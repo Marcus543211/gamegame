7046c17c3b61b60dcce56a3e00c66f6217372392
Marcus <35402586+Marcus543211@users.noreply.github.com>
2021-12-08 12:22:42 +0100

    Players now don't share a position

--- player.py
+++ player.py
@@ -1,18 +1,19 @@
 import pygame
 from pygame import Vector2
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
 
 @dataclass
 class Player:
-    acceleration: Vector2 = Vector2(0, 0)
-    velocity: Vector2 = Vector2(0, 0)
-    position: Vector2 = Vector2(0, 0)
+    acceleration: Vector2 = field(default_factory=lambda: Vector2(0, 0))
+    velocity: Vector2 = field(default_factory=lambda: Vector2(0, 0))
+    position: Vector2 = field(default_factory=lambda: Vector2(0, 0))
     force: float = 300
     drag: float = 0.006
     #static_drag: float = 0.75
 
-    def __init__(self):
+    def __post_init__(self):
         self.debug = True
         self.font = pygame.font.SysFont('MS UI Gothic', 20)
         self.last_acceleration = Vector2(0, 0)
@@ -23,16 +24,20 @@ class Player:
 
     @property
     def input_vector(self):
-        input = Vector2(0, 0)
-
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]: input -= Vector2(0, 1)
-        if keys[pygame.K_a]: input -= Vector2(1, 0)
-        if keys[pygame.K_s]: input += Vector2(0, 1)
-        if keys[pygame.K_d]: input += Vector2(1, 0)
-        
-        if input.length() != 0:
-            return input.normalize()
+
+        direction = Vector2(0, 0)
+        if keys[pygame.K_w]:
+            direction -= Vector2(0, 1)
+        if keys[pygame.K_a]:
+            direction -= Vector2(1, 0)
+        if keys[pygame.K_s]:
+            direction += Vector2(0, 1)
+        if keys[pygame.K_d]:
+            direction += Vector2(1, 0)
+
+        if direction.length() != 0:
+            return direction.normalize()
         else:
             return Vector2(0, 0)
 
@@ -44,7 +49,7 @@ class Player:
         dragForce = self.velocity.length() ** 2 * self.drag
         if self.velocity.length() != 0:
             self.acceleration -= self.velocity.normalize() * dragForce
-        
+
         # Acceleration, velocity and position
         self.velocity += self.acceleration * deltatime
         self.position += self.velocity * deltatime
@@ -57,6 +62,9 @@ class Player:
         pygame.draw.circle(screen, (255, 0, 0), self.position, 50)
 
         if self.debug:
-            screen.blit(self.font.render(f"Position: {self.position}", False, (0, 0, 0)), (0, 0))
-            screen.blit(self.font.render(f"Velocity: {self.velocity}", False, (0, 0, 0)), (0, 20))
-            screen.blit(self.font.render(f"Acceleration: {self.last_acceleration}", False, (0, 0, 0)), (0, 40))
+            screen.blit(self.font.render(
+                f"Position: {self.position}", False, (0, 0, 0)), (0, 0))
+            screen.blit(self.font.render(
+                f"Velocity: {self.velocity}", False, (0, 0, 0)), (0, 20))
+            screen.blit(self.font.render(
+                f"Acceleration: {self.last_acceleration}", False, (0, 0, 0)), (0, 40))
